54411c690ad7da0bf4fd51af9620cf230821bf66
David Carew <david@dcarew.com>
2022-05-18 13:16:41 -0700

    Add line class

---
+++ src/line.ts
@@ -0,0 +1,25 @@
+import {Point} from './point'
+import {Properties} from './properties'
+
+class Line extends Properties{
+    start: Point;
+    end: Point;
+
+    constructor(start: Point, end: Point){
+        super();
+        this.start = start;
+        this.end = end;
+    }
+
+    exportSVG(): string{
+        return `<line x1="${this.start.x}" y1="${this.start.y}" x2="${this.end.x}" y2="${this.end.y}" ${this.properties()}/>`
+    }
+
+    draw(canvas: CanvasRenderingContext2D){
+        canvas.strokeStyle = this.attributes.get('stroke');
+        canvas.beginPath();
+        canvas.moveTo(this.start.x, this.start.y);
+        canvas.lineTo(this.end.x, this.end.y);
+        canvas.stroke();
+    }
+}
