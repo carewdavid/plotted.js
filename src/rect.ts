aad89fd50c62d250cd1711fd1641c98748370b2c
David Carew <david@dcarew.com>
2022-06-01 20:29:57 -0700

    Add path to Rect to draw the stroke with

--- src/rect.ts
+++ src/rect.ts
@@ -1,16 +1,35 @@
 import {Properties} from './properties'
 import {Point} from './point'
+import { Path } from './path';
 
 export class Rect extends Properties{
     position: Point;
     width: number;
     height: number;
+    strokePath: Path;
+    nw: Point;
+    ne: Point;
+    se: Point;
+    sw: Point;
+
 
     constructor(position: Point, width: number, height: number){
         super();
         this.position = position;
         this.width = width;
         this.height = height;
+
+        const vertical = new Point(0, height);
+        const horizontal = new Point(width, 0);
+
+        //Set corners
+        this.nw = position;
+        this.ne = position.add(horizontal);
+        this.sw = position.add(vertical);
+        this.se = position.add(horizontal).add(vertical);
+
+        this.strokePath = new Path([this.nw, this.ne, this.se, this.sw]).close();
+
     }
 
     exportSVG(): string{
@@ -18,8 +37,10 @@ export class Rect extends Properties{
     }
 
     draw(canvas: CanvasRenderingContext2D){
-        canvas.strokeStyle = this.attributes.get('stroke');
         canvas.fillStyle = this.attributes.get('fill');
         canvas.fillRect(this.position.x, this.position.y, this.width, this.height);
+        this.strokePath.fill('none')
+        this.strokePath.stroke(this.attributes.get('stroke'));
+        this.strokePath.draw(canvas);
     }
 }
