0acabb89ec039d1486cc7e6d2ee725936b538734
David Carew <david@dcarew.com>
2023-01-04 10:46:31 -0800

    Add circle rendering

--- src/circle.ts
+++ src/circle.ts
@@ -12,5 +12,24 @@ export class Circle extends Properties {
         this.center = center;
         this.radius = radius;
     }
+
+    exportSVG(): string {
+        return `<circle cx="${this.center.x}" cy="${this.center.y}" r="${this.radius}" ${this.properties()}/>`;
+    }
+
+    draw(canvas: CanvasRenderingContext2D) {
+        let fill = this.attributes.get('fill') || 'none';
+        //canvas.lineWidth = Number.parseFloat(this.attributes.get('strokeWidth')) || 1;
+        if(fill !== 'none'){
+        canvas.fillStyle = fill;
+        canvas.beginPath();
+        canvas.arc(this.center.x, this.center.y, this.radius, 0, Math.PI * 2);
+        canvas.fill();
+        }
+        canvas.strokeStyle = this.attributes.get('stroke') || 'black';
+        canvas.beginPath();
+        canvas.arc(this.center.x, this.center.y, this.radius, 0, Math.PI * 2);
+        canvas.stroke();
+    }
 }
 
